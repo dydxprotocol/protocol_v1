d72e88865dfbbf196a35d85e2ee3c2e68d7fdff7
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-01-17 11:55:12 -0800

    Adding code coverage

---
+++ .solcover.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // norpc: true,
+  testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
+  compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
+  copyPackages: ['zeppelin-solidity']
+};
--- truffle.js
+++ truffle.js
@@ -8,6 +8,13 @@ module.exports = {
       port: 8545,
       network_id: "*",
       gas:4700000
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
     }
   },
   mocha: {
