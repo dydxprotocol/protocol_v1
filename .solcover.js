2a8ee2bde8621a354ba1b622166b0bc014555ba7
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-01-17 12:20:25 -0800

    up

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,4 @@
 module.exports = {
-  // norpc: true,
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
   copyPackages: ['zeppelin-solidity']
