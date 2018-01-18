aa9cc818ce8eca94211a7d819732d117258ee8c9
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-01-17 16:44:47 -0800

    ignore

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,6 @@
 module.exports = {
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-  copyPackages: ['zeppelin-solidity']
+  copyPackages: ['zeppelin-solidity'],
+  skipFiles: ['0x/', 'interfaces/', 'option/', 'testing/']
 };
