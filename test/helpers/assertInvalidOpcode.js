fcfebc3c5b257a0c48405afbb8c77ff84dba6032
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-02-14 13:58:21 -0800

    Allow owner to put ShortSell into close only mode

--- test/helpers/assertInvalidOpcode.js
+++
@@ -1,8 +0,0 @@
-const expect = require('chai').expect;
-
-module.exports = function(error) {
-  expect(
-    error.message.search('Exception while processing transaction: revert'),
-    'revert error must be returned'
-  ).to.be.at.least(0);
-}
