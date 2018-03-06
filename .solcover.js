600b23435a13b523bd8eff79a6ec122af534db26
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-03-06 10:25:19 -0800

    Fix Coverage (#105)
    Add coverage report for solidity code

--- .solcover.js
+++ .solcover.js
@@ -2,5 +2,5 @@ module.exports = {
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
   copyPackages: ['zeppelin-solidity'],
-  skipFiles: ['0x/', 'interfaces/', 'option/', 'testing/']
+  skipFiles: ['0x/', 'interfaces/', 'option/', 'testing/', 'shared/Exchange.sol']
 };
