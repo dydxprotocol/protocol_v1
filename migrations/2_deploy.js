c497b7650f44566d69a9b011e2ed8bb7eba2101a
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-03-05 22:00:42 -0800

    Fix coverage

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -24,7 +24,8 @@ function isDevNetwork(network) {
   return network === 'development'
           || network === 'test'
           || network === 'develop'
-          || network === 'dev';
+          || network === 'dev'
+          || network === 'coverage';
 }
 
 function maybeDeployTestTokens(deployer, network) {
