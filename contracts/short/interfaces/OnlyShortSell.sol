e404de76210abe869a516c0b53ccae4f6f49611b
Brendan Chou <brendanchou@msn.com>
2018-03-07 15:47:08 -0800

    add OnlyShortSell

---
+++ contracts/short/interfaces/OnlyShortSell.sol
@@ -0,0 +1,41 @@
+pragma solidity 0.4.19;
+
+
+/**
+ * @title OnlyShortSell
+ * @author dYdX
+ *
+ * Contract to store the address of the main ShortSell contract and trust only that address to call
+ * certain functions.
+ */
+contract OnlyShortSell {
+
+    // -----------------------
+    // ------ Constants ------
+    // -----------------------
+
+    // address of the known and trusted ShortSell contract on the blockchain
+    address public SHORT_SELL;
+
+    // -------------------------
+    // ------ Constructor ------
+    // -------------------------
+
+    function OnlyShortSell(
+        address _shortSell
+    )
+        public
+    {
+        SHORT_SELL = _shortSell;
+    }
+
+    // ---------------------------
+    // -------- Modifiers --------
+    // ---------------------------
+
+    modifier onlyShortSell()
+    {
+        require(msg.sender == SHORT_SELL);
+        _;
+    }
+}
