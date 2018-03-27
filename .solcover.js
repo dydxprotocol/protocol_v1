d7802d752ff286784a97051394e5d61a071be68a
Brendan Chou <brendanchou@msn.com>
2018-03-27 16:03:15 -0700

    ignore lib

--- .solcover.js
+++ .solcover.js
@@ -2,5 +2,5 @@ module.exports = {
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
   copyPackages: ['zeppelin-solidity'],
-  skipFiles: ['0x/', 'interfaces/', 'testing/']
+  skipFiles: ['0x/', 'interfaces/', 'lib/', 'testing/']
 };
