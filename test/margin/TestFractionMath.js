9fe6dcb473968ae3a03e7ccc8ef705c7f8eb5909
Brendan Chou <brendanchou@msn.com>
2018-05-08 16:51:34 -0700

    Improve FractionMath, add some tests (#270)
    * add more fractionMath testing, decrease gas costs, improve bounding accuracy by up to a factor of 2

--- test/margin/TestFractionMath.js
+++ test/margin/TestFractionMath.js
@@ -6,19 +6,50 @@ chai.use(require('chai-bignumber')());
 
 const TestFractionMath = artifacts.require("TestFractionMath");
 const { BIGNUMBERS } = require('../helpers/Constants');
+const { expectAssertFailure } = require('../helpers/ExpectHelper');
+
+const bn = BIGNUMBERS.ONES_127;
 
 contract('FractionMath', function(_accounts) {
   let contract;
 
-  before('', async () => {
+  before('deploy mock contract', async () => {
     contract = await TestFractionMath.new();
   });
 
   describe('#add', () => {
     it('succeeds for addition overflow', async () => {
-      const bn = BIGNUMBERS.ONES_127;
-      const [num, den] = await contract.add(bn,bn,bn,bn);
+      const [num, den] = await contract.add(bn, bn, bn, bn);
       expect(num).to.be.bignumber.equal(den.times(2));
+    });
+  });
+
+  describe('#sub1Over', () => {
+    async function sub1Over(num, den, d, numRes, denRes) {
+      const result = await contract.sub1Over(num, den, d);
+      expect(result[0]).to.be.bignumber.equal(numRes);
+      expect(result[1]).to.be.bignumber.equal(denRes);
+    }
+
+    async function sub1OverThrow(num, den, d) {
+      await expectAssertFailure(contract.sub1Over(num, den, d));
+    }
+
+    it('succeeds for sub1over', async () => {
+      await Promise.all([
+        sub1Over(3, 5, 2, 1, 10),
+        sub1Over(2, 6, 3, 0, 6),
+        sub1Over(1, 2, 4, 2, 8),
+        sub1Over(1, 2, 3, 1, 6),
+        sub1Over(1, 3, 5, 2, 15),
+        sub1Over(bn, bn, 2, bn.div(2).floor(), bn),
+      ]);
+    });
+
+    it('fails for bad values', async () => {
+      await Promise.all([
+        sub1OverThrow(1, 4, 2),
+      ]);
     });
   });
 });
