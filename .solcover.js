1915f5886aef3aa3e796eaf565ff81fbed5eeb21
Brendan Chou <brendanchou@msn.com>
2018-05-16 18:57:32 -0700

    simplify solcover

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,6 @@ module.exports = {
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
   copyPackages: ['zeppelin-solidity'],
-  skipFiles: ['external/0x/', 'external/Kyber/', 'testing/'],
+  skipFiles: ['external/', 'testing/'],
   copyNodeModules: true
 };
