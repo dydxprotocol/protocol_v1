ded1b181e79c81b2b58b8e250bdab4e6ce4acfc1
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-05-18 12:54:55 -0700

    Remove Kyber Contracts (#307)

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,5 @@ module.exports = {
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
   copyPackages: ['zeppelin-solidity'],
-  skipFiles: ['external/', 'testing/'],
-  copyNodeModules: true
+  skipFiles: ['external/', 'testing/']
 };
--- contracts/external/kyber/WhiteListInterface.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.4.23;
-pragma experimental "v0.5.0";
-
-
-contract WhiteListInterface {
-    function getUserCapInWei(address user) external view returns (uint userCapWei);
-}
