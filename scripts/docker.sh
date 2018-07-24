0c817930fbbe6ddd1818dd529b6ab0a6bdc6de2c
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-07-23 17:15:14 -0700

    Reset the migration in docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -10,7 +10,7 @@ trap cleanup SIGINT SIGTERM
 
 npm run node -- -i 1212 -d -p 8545 -h 0.0.0.0 &
 sleep 5
-npm run migrate -- --network=docker
+npm run migrate -- --network=docker --reset
 
 while [ 1 ]
 do
