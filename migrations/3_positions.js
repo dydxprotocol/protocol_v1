507419de786c7949536a4158d703ad740d576195
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-08-07 14:54:40 -0700

    test

--- migrations/3_positions.js
+++ migrations/3_positions.js
@@ -13,25 +13,25 @@ web3.currentProvider.sendAsync = web3.currentProvider.send;
 const writeFileAsync = promisify(fs.writeFile);
 
 async function doMigration(deployer, network, accounts) {
-  // if (isDevNetwork(network)) {
-  //   let salt = 729436712;
-  //   const openTransactions = [];
-  //
-  //   openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
-  //   openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
-  //   openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
-  //
-  //   await snapshot();
-  //   const margin = await Margin.deployed();
-  //   const positions = await Promise.all(openTransactions.map(t => getPosition(margin, t.id)));
-  //   for (let i = 0; i < openTransactions.length; i++) {
-  //     positions[i].id = openTransactions[i].id;
-  //   }
-  //
-  //   const json = JSON.stringify(positions, null, 4);
-  //
-  //   await writeFileAsync(__dirname + '/../build/test-positions.json', json, 'utf8');
-  // }
+  if (isDevNetwork(network)) {
+    let salt = 729436712;
+    const openTransactions = [];
+
+    openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
+    openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
+    openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
+
+    await snapshot();
+    const margin = await Margin.deployed();
+    const positions = await Promise.all(openTransactions.map(t => getPosition(margin, t.id)));
+    for (let i = 0; i < openTransactions.length; i++) {
+      positions[i].id = openTransactions[i].id;
+    }
+
+    const json = JSON.stringify(positions, null, 4);
+
+    await writeFileAsync(__dirname + '/../build/test-positions.json', json, 'utf8');
+  }
 }
 
 module.exports = (deployer, network, accounts) => {
