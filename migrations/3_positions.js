18431bd6d93d6fb280cd4f4629bc8abb540abb61
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-08-07 17:44:57 -0700

    fix snapshots

--- migrations/3_positions.js
+++ migrations/3_positions.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const promisify = require("es6-promisify");
 const Margin = artifacts.require('Margin');
 const { isDevNetwork } = require('./helpers');
-const { snapshot } = require('../test/helpers/SnapshotHelper');
+const { snapshot } = require('../src/snapshots');
 const { doOpenPosition, getPosition } = require('../test/helpers/MarginHelper');
 
 web3.currentProvider.sendAsync = web3.currentProvider.send;
@@ -21,7 +21,8 @@ async function doMigration(deployer, network, accounts) {
     openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
     openTransactions.push(await doOpenPosition(accounts, { salt: salt++ }));
 
-    await snapshot();
+    await snapshot(web3);
+
     const margin = await Margin.deployed();
     const positions = await Promise.all(openTransactions.map(t => getPosition(margin, t.id)));
     for (let i = 0; i < openTransactions.length; i++) {
--- src/test-positions.js
+++ src/test-positions.js
@@ -1,18 +1,5 @@
-import promisify from "es6-promisify";
 import BigNumber from 'bignumber.js';
 import TestPositionsJson from '../build/test-positions.json';
-
-export function reset(web3Instance) {
-  // Needed for different versions of web3
-  const func = web3Instance.currentProvider.sendAsync || web3Instance.currentProvider.send;
-
-  return promisify(func)({
-    jsonrpc: "2.0",
-    method: "evm_revert",
-    id: 12345,
-    params: ['0x1'],
-  });
-}
 
 export const TEST_POSITIONS = TestPositionsJson.map(mapPositionFromJson);
 
