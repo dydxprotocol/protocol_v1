a126221b7020ac0b8ed68a472ed12f14373cb7f9
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-08-15 16:46:09 -0700

    Use provider when snapshotting (#417)

--- test/tests/js/TestSeeds.js
+++ test/tests/js/TestSeeds.js
@@ -3,19 +3,17 @@ const { getPosition } = require('../../helpers/MarginHelper');
 const chai = require('chai');
 const expect = chai.expect;
 chai.use(require('chai-bignumber')());
-const BigNumber = require('bignumber.js');
 const { ZeroEx } = require('0x.js');
 
 const Margin = artifacts.require('Margin');
-const TokenA = artifacts.require('TokenA');
 const TestToken = artifacts.require('TestToken');
 const ZeroExProxy = artifacts.require('ZeroExProxy');
 
-contract('Margin', accounts => {
+contract('Margin', () => {
   after(async () => {
-    await reset(web3);
+    await reset(web3.currentProvider);
     // Snapshot a final time to fool truffle into reverting to this in the contract block
-    await snapshot(web3);
+    await snapshot(web3.currentProvider);
   });
 
   describe('seeds', () => {
@@ -27,7 +25,7 @@ contract('Margin', accounts => {
       it('seed positions still exist after reset', async () => {
         await checkSeedPositions();
 
-        await reset(web3);
+        await reset(web3.currentProvider);
 
         await checkSeedPositions();
       });
@@ -41,53 +39,10 @@ contract('Margin', accounts => {
       it('seed orders still exist after reset', async () => {
         await checkSeedOrders();
 
-        await reset(web3);
+        await reset(web3.currentProvider);
 
         await checkSeedOrders();
       });
-    });
-  });
-
-  describe('#reset', () => {
-    it('resets any transactions made', async () => {
-      const account = accounts[5];
-      const amount = new BigNumber(123456);
-      const token = await TokenA.deployed();
-
-      const startingBalance = await token.balanceOf.call(account);
-      await token.issueTo(account, amount);
-      const afterBalance = await token.balanceOf.call(account);
-
-      expect(afterBalance).to.be.bignumber.eq(startingBalance.plus(amount));
-
-      await reset(web3);
-
-      const balanceAfterReset = await token.balanceOf.call(account);
-      expect(balanceAfterReset).to.be.bignumber.eq(startingBalance);
-    });
-
-    it('works multiple times', async () => {
-      const account = accounts[5];
-      const amount = new BigNumber(123456);
-      const token = await TokenA.deployed();
-
-      const startingBalance = await token.balanceOf.call(account);
-      await token.issueTo(account, amount);
-      const afterBalance = await token.balanceOf.call(account);
-
-      expect(afterBalance).to.be.bignumber.eq(startingBalance.plus(amount));
-
-      await reset(web3);
-
-      let balanceAfterReset = await token.balanceOf.call(account);
-      expect(balanceAfterReset).to.be.bignumber.eq(startingBalance);
-
-      await token.issueTo(account, amount);
-
-      await reset(web3);
-
-      balanceAfterReset = await token.balanceOf.call(account);
-      expect(balanceAfterReset).to.be.bignumber.eq(startingBalance);
     });
   });
 });
