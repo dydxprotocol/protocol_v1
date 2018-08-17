9c1ae8f48239ecf240a12d3c550672070f9d8d1a
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-08-17 12:45:09 -0700

    Upgrade + Fix Build Script (#419)
    * Upgrade + Fix Build Script
    
    * extra

--- scripts/prod_build.sh
+++ scripts/prod_build.sh
@@ -8,5 +8,5 @@ npm run migrate -- --network=docker --reset
 kill -TERM $PROC_ID
 
 npm run clean_contract_json
-rm -rf ../build/contracts
+rm -rf ./build/contracts
 mv ./build/test ./build/contracts
