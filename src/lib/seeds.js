8221bcd4751e2277dab484ecc9eb20a7d1de90b8
Antonio Juliano <antonio.m.juliano@gmail.com>
2018-08-30 14:31:12 -0700

    fix typo in seeds (#429)
    * fix typo in seeds
    
    * 0.2.13
    
    * bal

--- src/lib/seeds.js
+++ src/lib/seeds.js
@@ -27,7 +27,7 @@ export default SeedsJson;
 function mapPositionFromJson(jsonPosition) {
   return {
     ...jsonPosition,
-    position: new BigNumber(jsonPosition.principal),
+    principal: new BigNumber(jsonPosition.principal),
     interestRate: new BigNumber(jsonPosition.interestRate),
     requiredDeposit: new BigNumber(jsonPosition.requiredDeposit),
     callTimeLimit: new BigNumber(jsonPosition.callTimeLimit),
@@ -35,6 +35,7 @@ function mapPositionFromJson(jsonPosition) {
     startTimestamp: new BigNumber(jsonPosition.startTimestamp),
     maxDuration: new BigNumber(jsonPosition.maxDuration),
     interestPeriod: new BigNumber(jsonPosition.interestPeriod),
+    balance: new BigNumber(jsonPosition.balance),
   }
 }
 
